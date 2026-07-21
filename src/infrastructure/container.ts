a69f0f9b99078db5bc095ac43c74d945267a27b2
exp-KiTaku <r123313@g.kobe-kosen.ac.jp>
2026-07-21 16:13:00 +0900

    例示を少しわかりやすく

--- src/infrastructure/container.ts
+++ src/infrastructure/container.ts
@@ -1,9 +1,11 @@
 import "reflect-metadata";
 import { Container } from "inversify";
+import type { ICategoryRepository } from "../repositories/ICategoryRepository";
 import { TYPES } from "./types";
 
 const container = new Container();
 
-// container.bind<本実装クラス>(TYPES.[types.tsに登録したシンボル]).to(本実装クラス);
+// container.bind<インターフェース>(TYPES.[types.tsに登録したシンボル]).to(実装クラス); のようにする
+//container.bind<ICategoryRepository>(TYPES.ICategoryRepository).to(CategoryRepository);
 
 export { container };
