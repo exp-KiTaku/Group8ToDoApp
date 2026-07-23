3722880a91a3f08cd965854c367f9042f5b6630a
HikaruHashimoto <r123334@g.kobe-kosen.ac.jp>
2026-07-24 01:30:30 +0900

    DIコンテナ紐づけ

--- src/infrastructure/container.ts
+++ src/infrastructure/container.ts
@@ -1,9 +1,9 @@
 import "reflect-metadata";
 import { Container } from "inversify";
 import type { ICategoryRepository } from "../repositories/ICategoryRepository";
-import { InMemoryCategoryRepository } from "../repositories/InMemoryCategoryRepository";
+import { LocalStorageCategoryRepository } from "../repositories/LocalStorageCategoryRepository";
 import type { ITaskRepository } from "../repositories/ITaskRepository";
-import { InMemoryTaskRepository } from "../repositories/InMemoryTaskRepository";
+import { LocalStorageTaskRepository } from "../repositories/LocalStorageTaskRepository";
 import type { ICategoryService } from "../services/ICategoryService";
 import { CategoryService } from "../services/CategoryService";
 import type { ITaskService } from "../services/ITaskService";
@@ -12,8 +12,8 @@ import { TYPES } from "./types";
 
 const container = new Container();
 
-container.bind<ICategoryRepository>(TYPES.ICategoryRepository).to(InMemoryCategoryRepository).inSingletonScope();
-container.bind<ITaskRepository>(TYPES.ITaskRepository).to(InMemoryTaskRepository).inSingletonScope();
+container.bind<ICategoryRepository>(TYPES.ICategoryRepository).to(LocalStorageCategoryRepository).inSingletonScope();
+container.bind<ITaskRepository>(TYPES.ITaskRepository).to(LocalStorageTaskRepository).inSingletonScope();
 container.bind<ICategoryService>(TYPES.ICategoryService).to(CategoryService).inSingletonScope();
 container.bind<ITaskService>(TYPES.ITaskService).to(TaskService).inSingletonScope();
 
