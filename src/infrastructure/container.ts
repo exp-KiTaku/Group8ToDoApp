984f0a4c9ba1ad6136fd39114d29ab5292a891a3
exp-KiTaku <r123313@g.kobe-kosen.ac.jp>
2026-07-23 14:35:23 +0900

    DIコンテナ整理

--- src/infrastructure/container.ts
+++ src/infrastructure/container.ts
@@ -1,21 +1,17 @@
 import "reflect-metadata";
 import { Container } from "inversify";
 import type { ICategoryRepository } from "../repositories/ICategoryRepository";
+import { InMemoryCategoryRepository } from "../repositories/InMemoryCategoryRepository";
+import type { ITaskRepository } from "../repositories/ITaskRepository";
+import { InMemoryTaskRepository } from "../repositories/InMemoryTaskRepository";
 import type { ICategoryService } from "../services/ICategoryService";
 import { CategoryService } from "../services/CategoryService";
 import type { ITaskService } from "../services/ITaskService";
 import { TaskService } from "../services/TaskService";
 import { TYPES } from "./types";
 
-import type { ITaskRepository } from "../repositories/ITaskRepository";
-
-import { InMemoryCategoryRepository } from "../repositories/InMemoryCategoryRepository";
-import { InMemoryTaskRepository } from "../repositories/InMemoryTaskRepository";
-
 const container = new Container();
 
-// container.bind<インターフェース>(TYPES.[types.tsに登録したシンボル]).to(実装クラス); のようにする
-//container.bind<ICategoryRepository>(TYPES.ICategoryRepository).to(CategoryRepository);
 container.bind<ICategoryRepository>(TYPES.ICategoryRepository).to(InMemoryCategoryRepository);
 container.bind<ITaskRepository>(TYPES.ITaskRepository).to(InMemoryTaskRepository);
 container.bind<ICategoryService>(TYPES.ICategoryService).to(CategoryService);
