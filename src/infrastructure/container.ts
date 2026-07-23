b50a3b75f8c4d6718585bcbafc77e4db3222224b
StarMine888 <r123311@g.kobe-kosen.ac.jp>
2026-07-23 14:15:34 +0900

    InMemoryRepositoryをDIコンテナに登録

--- src/infrastructure/container.ts
+++ src/infrastructure/container.ts
@@ -7,10 +7,17 @@ import type { ITaskService } from "../services/ITaskService";
 import { TaskService } from "../services/TaskService";
 import { TYPES } from "./types";
 
+import type { ITaskRepository } from "../repositories/ITaskRepository";
+
+import { InMemoryCategoryRepository } from "../repositories/InMemoryCategoryRepository";
+import { InMemoryTaskRepository } from "../repositories/InMemoryTaskRepository";
+
 const container = new Container();
 
 // container.bind<インターフェース>(TYPES.[types.tsに登録したシンボル]).to(実装クラス); のようにする
 //container.bind<ICategoryRepository>(TYPES.ICategoryRepository).to(CategoryRepository);
+container.bind<ICategoryRepository>(TYPES.ICategoryRepository).to(InMemoryCategoryRepository);
+container.bind<ITaskRepository>(TYPES.ITaskRepository).to(InMemoryTaskRepository);
 container.bind<ICategoryService>(TYPES.ICategoryService).to(CategoryService);
 container.bind<ITaskService>(TYPES.ITaskService).to(TaskService);
 
